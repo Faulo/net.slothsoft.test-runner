c0e21418e5bfd8261fc03ab7e2e1009b1c438372
Daniel Schulz <faulolio@gmail.com>
2025-06-02 13:58:45 +0200

    feat: TestObjectStore

---
+++ Runtime/TestObjectStore.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace Slothsoft.TestRunner {
+    public class TestObjectStore : IDisposable {
+        readonly List<UnityObject> instances = new();
+        readonly bool destroyImmediately;
+
+        public TestObjectStore() : this(!Application.isPlaying) {
+        }
+
+        public TestObjectStore(bool destroyImmediately) {
+            this.destroyImmediately = destroyImmediately;
+        }
+
+        ~TestObjectStore() {
+            if (instances.Count > 0) {
+                Debug.LogError($"A previous {this} was not disposed of properly!");
+            }
+        }
+
+        public T CreateUnityObject<T>(string name = default) where T : UnityObject, new() {
+            T instance = new() {
+                name = string.IsNullOrEmpty(name) ? "New " + ObjectNames.NicifyVariableName(typeof(T).Name) : name,
+                hideFlags = HideFlags.DontSave
+            };
+            instances.Add(instance);
+            return instance;
+        }
+
+        public T CreateScriptableObject<T>(string name = default) where T : ScriptableObject {
+            var instance = ScriptableObject.CreateInstance<T>();
+            instance.name = string.IsNullOrEmpty(name) ? "New " + ObjectNames.NicifyVariableName(typeof(T).Name) : name;
+            instance.hideFlags = HideFlags.DontSave;
+            instances.Add(instance);
+            return instance;
+        }
+
+        public GameObject CreateGameObject(string name = default, Vector3? position = default, Quaternion? rotation = default) {
+            GameObject instance = string.IsNullOrEmpty(name)
+                ? new()
+                : new(name);
+
+            if (position.HasValue) {
+                instance.transform.position = position.Value;
+            }
+
+            if (rotation.HasValue) {
+                instance.transform.rotation = rotation.Value;
+            }
+
+            instances.Add(instance);
+
+            return instance;
+        }
+
+        public GameObject CreatePrimitive(PrimitiveType type, Vector3? position = default, Quaternion? rotation = default) {
+            var instance = GameObject.CreatePrimitive(type);
+            if (position.HasValue) {
+                instance.transform.position = position.Value;
+            }
+
+            if (rotation.HasValue) {
+                instance.transform.rotation = rotation.Value;
+            }
+
+            instances.Add(instance);
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Destroys all objects created by this instance.
+        /// </summary>
+        public void Dispose() {
+            foreach (var instance in instances) {
+                if (instance) {
+                    if (destroyImmediately) {
+                        UnityObject.DestroyImmediate(instance);
+                    } else {
+                        UnityObject.Destroy(instance);
+                    }
+                }
+            }
+
+            instances.Clear();
+        }
+    }
+}
