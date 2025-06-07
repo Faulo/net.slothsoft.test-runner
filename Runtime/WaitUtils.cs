df05890cb708fcdd0ad0662ef7e7f269c15696b9
Daniel Schulz <faulolio@gmail.com>
2025-06-07 13:31:52 +0200

    build: remove dependency on net.slothsoft.unity-extensions

--- Runtime/WaitUtils.cs
+++ Runtime/WaitUtils.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections;
 using NUnit.Framework;
-using Slothsoft.UnityExtensions;
 using UnityEngine;
 
 namespace Slothsoft.TestRunner {
     public static class WaitUtils {
+        static readonly WaitForFixedUpdate forFixedUpdate = new();
+
         public static IEnumerator WaitFor(Func<bool> predicate, string message = default, float timeout = 1) {
             float timer = 0;
             while (!predicate() && timer < timeout) {
                 timer += Time.fixedDeltaTime;
-                yield return Wait.forFixedUpdate;
+                yield return forFixedUpdate;
             }
 
             Assert.That(predicate(), Is.True, message ?? $"Waited {timeout} second(s) in vain!");
