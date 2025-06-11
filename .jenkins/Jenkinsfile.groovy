9b7c370fcc1ab3829539eda995969112a7a69f57
Daniel Schulz <faulolio@gmail.com>
2025-06-11 18:50:07 +0200

    devops: fix syntax

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -11,27 +11,27 @@ pipeline {
 			steps {
 				script {
 					unityPackage(
-						// Define Unity package location relative to repository.
-						PACKAGE_LOCATION : '',
-						UNITY_NODE : 'unity && linux',
+							// Define Unity package location relative to repository.
+							PACKAGE_LOCATION : '',
+							UNITY_NODE : 'unity && linux',
 
-						// Assert that CHANGELOG.md has been updated.
-						TEST_CHANGELOG : '1',
-						CHANGELOG_LOCATION : 'CHANGELOG.md',
+							// Assert that CHANGELOG.md has been updated.
+							TEST_CHANGELOG : '1',
+							CHANGELOG_LOCATION : 'CHANGELOG.md',
 
-						// Assert Unity's Test Runner tests.
-						TEST_UNITY : '1',
-						TEST_MODES = 'EditMode PlayMode'
+							// Assert Unity's Test Runner tests.
+							TEST_UNITY : '1',
+							TEST_MODES : 'EditMode PlayMode',
 
-						// Assert that the C# code of the package matches the .editorconfig.
-						TEST_FORMATTING : '1',
-						EDITORCONFIG_LOCATION : '.jenkins/.editorconfig',
+							// Assert that the C# code of the package matches the .editorconfig.
+							TEST_FORMATTING : '1',
+							EDITORCONFIG_LOCATION : '.jenkins/.editorconfig',
 
-						// Deploy the package to a Verdaccio server.
-						DEPLOY_TO_VERDACCIO : '0',
-						VERDACCIO_URL : 'http://verdaccio',
-						VERDACCIO_CREDENTIALS : 'Slothsoft-Verdaccio',
-					)
+							// Deploy the package to a Verdaccio server.
+							DEPLOY_TO_VERDACCIO : '0',
+							VERDACCIO_URL : 'http://verdaccio',
+							VERDACCIO_CREDENTIALS : 'Slothsoft-Verdaccio',
+							)
 				}
 			}
 		}
@@ -39,27 +39,27 @@ pipeline {
 			steps {
 				script {
 					unityPackage(
-						// Define Unity package location relative to repository.
-						PACKAGE_LOCATION : '',
-						UNITY_NODE : 'unity && windows',
+							// Define Unity package location relative to repository.
+							PACKAGE_LOCATION : '',
+							UNITY_NODE : 'unity && windows',
 
-						// Assert that CHANGELOG.md has been updated.
-						TEST_CHANGELOG : '1',
-						CHANGELOG_LOCATION : 'CHANGELOG.md',
+							// Assert that CHANGELOG.md has been updated.
+							TEST_CHANGELOG : '1',
+							CHANGELOG_LOCATION : 'CHANGELOG.md',
 
-						// Assert Unity's Test Runner tests.
-						TEST_UNITY : '1',
-						TEST_MODES = 'EditMode PlayMode'
+							// Assert Unity's Test Runner tests.
+							TEST_UNITY : '1',
+							TEST_MODES : 'EditMode PlayMode',
 
-						// Assert that the C# code of the package matches the .editorconfig.
-						TEST_FORMATTING : '1',
-						EDITORCONFIG_LOCATION : '.jenkins/.editorconfig',
+							// Assert that the C# code of the package matches the .editorconfig.
+							TEST_FORMATTING : '1',
+							EDITORCONFIG_LOCATION : '.jenkins/.editorconfig',
 
-						// Deploy the package to a Verdaccio server.
-						DEPLOY_TO_VERDACCIO : '1',
-						VERDACCIO_URL : 'http://verdaccio',
-						VERDACCIO_CREDENTIALS : 'Slothsoft-Verdaccio',
-					)
+							// Deploy the package to a Verdaccio server.
+							DEPLOY_TO_VERDACCIO : '1',
+							VERDACCIO_URL : 'http://verdaccio',
+							VERDACCIO_CREDENTIALS : 'Slothsoft-Verdaccio',
+							)
 				}
 			}
 		}
