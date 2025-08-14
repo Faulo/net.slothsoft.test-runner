f59d17c815873d8dfcee5aa78b04bf93b06c9d3e
Daniel Schulz <faulolio@gmail.com>
2025-08-14 14:53:20 +0200

    test: unseal

--- Tests/PlayMode/Runtime/AddSubstituteTests.cs
+++ Tests/PlayMode/Runtime/AddSubstituteTests.cs
@@ -8,7 +8,7 @@ namespace Slothsoft.TestRunner.Runtime {
         public abstract void Print(string message);
     }
 
-    sealed class ConcreteTestBehaviour : MonoBehaviour, ITestInterface {
+    class ConcreteTestBehaviour : MonoBehaviour, ITestInterface {
         public void Print(string message) {
         }
     }
