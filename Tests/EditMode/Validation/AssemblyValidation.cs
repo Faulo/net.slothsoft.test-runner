0479253ad8d6329c7089bdb896922a8fcba284f1
Daniel Schulz <faulolio@gmail.com>
2025-08-14 16:41:44 +0200

    fix: assembly

--- Tests/EditMode/Validation/AssemblyValidation.cs
+++ Tests/EditMode/Validation/AssemblyValidation.cs
@@ -4,11 +4,11 @@ using Slothsoft.TestRunner.Editor.Validation;
 namespace Slothsoft.TestRunner.Validation {
     sealed class AssemblyValidation : AssemblyValidationBase<AssemblyValidation.Source> {
         internal sealed class Source : IAssemblySource {
-            public IEnumerable<string> GetAssemblyNames() => new[] {
-                AssemblyInfo.NAMESPACE_RUNTIME,
-                AssemblyInfo.NAMESPACE_EDITOR,
-                AssemblyInfo.NAMESPACE_TESTS_PLAYMODE,
-                AssemblyInfo.NAMESPACE_TESTS_EDITMODE,
+            public IEnumerable<string> GetAssemblyNames() => new string[] {
+                // AssemblyInfo.NAMESPACE_RUNTIME,
+                // AssemblyInfo.NAMESPACE_EDITOR,
+                // AssemblyInfo.NAMESPACE_TESTS_PLAYMODE,
+                // AssemblyInfo.NAMESPACE_TESTS_EDITMODE,
             };
         }
     }
